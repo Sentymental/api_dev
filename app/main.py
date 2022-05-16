3946f952e18baf4cc42e1b325d98b57baffeb32f
Sentymental <luk.szwajnoch95@gmail.com>
2022-05-16 22:13:09 +0200

    Added Get Post by ID Endpoint

--- app/main.py
+++ app/main.py
@@ -26,27 +26,6 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
-my_posts: list[dict] = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "Pizza Lover", "content": "I Like pizza", "id": 2},
-]
-
-
-def find_post(post_id: int):
-    """Function that will loop over our posts and will look for specific ID"""
-    for post in my_posts:
-        if post["id"] == post_id:
-            return post
-        return None
-
-
-def find_index_post(post_id: int):
-    """Find corresponding index"""
-    for i, post in enumerate(my_posts):
-        if post["id"] == post_id:
-            return i
-
-
 # Path Operations:
 @app.get("/")
 async def root():
@@ -54,18 +33,19 @@ async def root():
     return {"message": "Hello World"}  # JSON
 
 
+# Get Single Post Endpoint:
 @app.get("/posts/{post_id}")  # Path parameter
-def get_post(post_id: int):
+def get_post(post_id: int, db_session: Session = Depends(get_db)):
     """GET method endpoint that points to provided post ID"""
-
-    post = find_post(post_id)
+    post = db_session.query(models.Post).filter(models.Post.id == post_id).first()
     if not post:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not Found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!"
         )
-    return {"post_detail": post}
+    return post
 
 
+# Get All Posts Endpoint:
 @app.get("/posts")
 def get_posts(db_session: Session = Depends(get_db)):
     """GET method endpoint that points to our posts"""
@@ -73,6 +53,7 @@ def get_posts(db_session: Session = Depends(get_db)):
     return posts
 
 
+# Post Creation Endpoint:
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post, db_session: Session = Depends(get_db)):
     """POST method endpoint that creates our posts"""
@@ -83,27 +64,27 @@ def create_posts(post: Post, db_session: Session = Depends(get_db)):
     return post
 
 
-@app.delete("/posts/{post_id}")
-def delete_post(post_id: int):
-    """Delete a post"""
-    index = find_index_post(post_id)
-    if not index:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!"
-        )
-    my_posts.pop(index)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+# @app.delete("/posts/{post_id}")
+# def delete_post(post_id: int):
+#     """Delete a post"""
+#     index = find_index_post(post_id)
+#     if not index:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!"
+#         )
+#     my_posts.pop(index)
+#     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{post_id}")
-def update_post(post_id: int, post: Post):
-    """Update post"""
-    index = find_index_post(post_id)
-    if not index:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!"
-        )
-    post_dict = post.dict()
-    post_dict["id"] = post_id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+# @app.put("/posts/{post_id}")
+# def update_post(post_id: int, post: Post):
+#     """Update post"""
+#     index = find_index_post(post_id)
+#     if not index:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!"
+#         )
+#     post_dict = post.dict()
+#     post_dict["id"] = post_id
+#     my_posts[index] = post_dict
+#     return {"data": post_dict}
