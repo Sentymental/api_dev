41671ad3166dd4acc1c668b6f2ffcce904bee5ef
Sentymental <luk.szwajnoch95@gmail.com>
2022-05-30 15:04:09 +0200

    Bug Fix: changed login HTTPException from 404 to 403 that will better represent current status

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -28,12 +28,12 @@ async def login(
 
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials"
+            status_code=status.HTTP_403_NOT_FOUND, detail="Invalid credentials"
         )
 
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials"
+            status_code=status.HTTP_403_NOT_FOUND, detail="Invalid credentials"
         )
 
     access_token = oauth2.create_access_token(data={"user_id": user.id})
