775e68649bdeb76cf316c9445ad5b473d544bab3
Sentymental <luk.szwajnoch95@gmail.com>
2022-05-30 15:02:05 +0200

    Bug Fix: oauth2.py datetime.now() changed to utcnow()

--- app/oauth2.py
+++ app/oauth2.py
@@ -15,7 +15,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 def create_access_token(data: dict):
     """Create access token"""
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -24,8 +24,8 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
     """Verify provided access token"""
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-        user_id = payload.get("users_id")
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("user_id")
 
         if user_id is None:
             raise credentials_exception
@@ -33,6 +33,7 @@ def verify_access_token(token: str, credentials_exception):
 
     except JWTError:
         raise credentials_exception
+    return token_data
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -36,6 +36,6 @@ async def login(
             status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials"
         )
 
-    access_token = oauth2.create_access_token(data={"users_id": user.id})
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
 
     return {"access_token": access_token, "token_type": "bearer"}
